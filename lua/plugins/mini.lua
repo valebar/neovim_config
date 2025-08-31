2eaa98d683495e8d8d40b2fddf618978bf7fe7b4
Vasja Lebar <vasja.lebar@pawsm.com>
2025-08-31 15:01:31 +0200

    updated mini & tokionight

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,4 +1,46 @@
 return { 
 	'echasnovski/mini.nvim', 
 	version = '*',
+	config = function()
+		require('mini.align').setup()
+
+		require('mini.bracketed').setup({
+			buffer = { options = { wrap = false } },
+			comment = { suffix = '', options = { wrap = false } },
+			conflict = { options = { wrap = false } },
+			diagnostic = { options = { wrap = false } },
+			file = { suffix = '', options = { wrap = false } },
+			indent = { options = { wrap = false } },
+			jump = { suffix = '', options = { wrap = false } },
+			location = { suffix = '', options = { wrap = false } },
+			oldfile = { suffix = '', options = { wrap = false } },
+			quickfix = { options = { wrap = false } },
+			treesitter = { suffix = '', options = { wrap = false } },
+			undo = { options = { wrap = false } },
+			window = { options = { wrap = false } },
+			yank = { options = { wrap = false } },
+		})
+
+		require('mini.comment').setup()
+
+		require('mini.move').setup()
+
+		require('mini.operators').setup({
+			evaluate = {
+				prefix = '',
+			},
+			exchange = {
+				prefix = '<leader>e',
+			},
+			multiply = {
+				prefix = '',
+			},
+			replace = {
+				prefix = '<leader>r',
+			},
+			sort = {
+				prefix = '',
+			},
+		})
+	end,
 }
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,11 +1,15 @@
 return {
-	{
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+	'folke/tokyonight.nvim',
+	priority = 1000,
+	lazy = vim.g.rg_color ~= 'tokyonight',
+	opts = {
+		style = 'night',
+		transparent = vim.g.rg_transparent,
+		styles = {
+			comments = { fg = '#7e86ae' },
+			keywords = { italic = false },
+			sidebars = vim.g.rg_transparent and 'transparent' or 'dark',
+			floats = vim.g.rg_transparent and 'transparent' or 'dark',
+		},
+	},
 }
