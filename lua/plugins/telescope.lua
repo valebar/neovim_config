04097c758fa8e319350812cc6f309ec54c9f7e33
Vasja Lebar <vasja.lebar@pawsm.com>
2025-08-31 15:17:07 +0200

    Updated telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,24 @@
+-- return {
+-- 	'nvim-telescope/telescope.nvim', tag='0.1.8',
+-- 	dependencies = { 'nvim-lua/plenary.nvim' },
+	-- keys = {
+	-- 	{ "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", desc = "Telescope find files" },
+	-- 	{ "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", desc = "Telescope live grep" },
+	-- 	{ "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", desc = "Telescope buffers" },
+	-- 	{ "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", desc = "Telescope help tags" },
+	-- }
+-- }
 return {
-	'nvim-telescope/telescope.nvim', tag='0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	dependencies = { 
+		'nvim-lua/plenary.nvim',
+		'nvim-treesitter/nvim-treesitter'
+	},
 	keys = {
 		{ "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", desc = "Telescope find files" },
 		{ "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", desc = "Telescope live grep" },
 		{ "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", desc = "Telescope buffers" },
 		{ "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", desc = "Telescope help tags" },
 	}
+
 }
