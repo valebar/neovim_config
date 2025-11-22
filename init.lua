25e4793bf1189cdd97ab1f4d7c9fe6909b3fe46d
Vasja Lebar <vasja.lebar@pawsm.com>
2025-11-22 08:46:27 +0100

    added new plugins & removed unused code

--- init.lua
+++ init.lua
@@ -9,32 +9,20 @@ require('mini.statusline').setup({
 })
 require('mini.surround').setup()
 require('mini.comment').setup()
--- if (package.config:sub(1,1) == '\\') then
 require('mini.icons').setup({
 	style="ascii"
 })
--- else
-	-- require('mini.icons').setup({
-		-- style="glyph"
-	-- })
--- end
 require('mini.diff').setup()
 require('mini.tabline').setup()
 require('mini.trailspace').setup()
 require('mini.extra').setup()
 require('mini.align').setup()
+require('mini.indentscope').setup()
+require('mini.notify').setup()
 
 vim.lsp.enable('clangdlsp')
 vim.lsp.enable('odinlsp')
 vim.lsp.enable('rustlsp')
 vim.lsp.enable('golsp')
--- vim.lsp.config('rust_analyzer', {
--- 	settings = {
--- 		['rust-analyzer'] = {
--- 			diagnostics = {
--- 				enable = false;
--- 			}
--- 		}
--- 	}
--- })
+
 vim.cmd[[colorscheme tokyonight-storm]]
