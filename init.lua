a0e52a843adea01112000001ab22c077ccee883f
Vasja Lebar <vasja.lebar@pawsm.com>
2025-12-18 05:24:38 +0100

    added some comments

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ vim.opt.exrc = true
 vim.opt.secure = true
 
 require("config.lazy")
-
+-- mini stuff
 require('mini.completion').setup()
 require('mini.statusline').setup({
 	use_icons = false
@@ -19,15 +19,18 @@ require('mini.extra').setup()
 require('mini.align').setup()
 require('mini.indentscope').setup()
 require('mini.notify').setup()
-
+-- lsp stuff
 vim.lsp.enable('clangdlsp')
 vim.lsp.enable('odinlsp')
 vim.lsp.enable('rustlsp')
 vim.lsp.enable('golsp')
 vim.lsp.enable('cmake')
 vim.lsp.enable('tsserver')
+-- colorscheme
 vim.cmd[[colorscheme tokyonight-storm]]
-
+-- custum keymaps
+-- jump to declaration
 vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+-- jump to definition
 vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
 
