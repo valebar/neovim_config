f308cc5d427bf670a06e4d879bb627960e25760b
Vasja Lebar <vasja.lebar@pawsm.com>
2025-12-18 20:11:19 +0100

    code cleanup

--- init.lua
+++ init.lua
@@ -4,23 +4,6 @@ vim.api.nvim_set_option("clipboard","unnamed")
 
 require("config.lazy")
 
--- require('mini.completion').setup()
--- require('mini.statusline').setup({
--- 	use_icons = false
--- })
--- require('mini.surround').setup()
--- require('mini.comment').setup()
--- require('mini.icons').setup({
--- 	style="ascii"
--- })
--- require('mini.diff').setup()
--- require('mini.tabline').setup()
--- require('mini.trailspace').setup()
--- require('mini.extra').setup()
--- require('mini.align').setup()
--- require('mini.indentscope').setup()
--- require('mini.notify').setup()
-
 vim.lsp.enable('clangdlsp')
 vim.lsp.enable('odinlsp')
 vim.lsp.enable('rustlsp')
