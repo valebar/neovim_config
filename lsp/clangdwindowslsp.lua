588509c9d28d8a45475bfed34091dc09162c432d
Vasja Lebar <vasja.lebar@pawsm.com>
2025-12-30 08:57:37 +0100

    Windows clang lsp

---
+++ lsp/clangdwindowslsp.lua
@@ -0,0 +1,7 @@
+return {
+	cmd = {
+		'clangd',
+		'--query-driver=C:/Program Files (x86)/Microsoft Visual Studio/*/VC/Tools/MSVC/*/bin/**/cl.exe'
+	},
+	filetypes = {'c', 'cpp', 'h', 'C', 'hpp'},
+}
