2c56fe7c1ef1d2778ce6b34b6aba7caffc85395f
Vasja Lebar <vasja.lebar@pawsm.com>
2026-01-20 18:41:59 +0100

    cleanup

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
-vim.opt.exrc = true
-vim.opt.secure = true
--- vim.api.nvim_set_option("clipboard","unnamed")
+vim.o.exrc   = true
+vim.o.secure = true
 
 require("config.lazy")
 
@@ -10,12 +9,11 @@ vim.lsp.enable('rustlsp')
 vim.lsp.enable('golsp')
 vim.lsp.enable('cmake')
 vim.lsp.enable('tsserver')
--- colorscheme
+
 vim.cmd[[colorscheme tokyonight-storm]]
--- custum keymaps
--- jump to declaration
+
 vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
--- jump to definition
 vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+
 vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
 vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
